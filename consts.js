fddea9bda51c43ccb59f51f07518bcad55070c2a
jharrop1 <77983621+jharrop1@users.noreply.github.com>
2021-12-08 12:47:07 -0500

    updated with login/register

---
+++ consts.js
@@ -0,0 +1,7 @@
+//const MONGODB_URL = 'mongodb://localhost:27017/movieratings';
+const MONGODB_URL = 'mongodb+srv://jharrop:webdev@cluster0.uq4iv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//username for mongodb is jharrop pw is webdev
+
+module.exports = {
+    MONGODB_URL
+};
--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+const CONSTANTS = require('./consts');
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:3000"); //add in netlify link to Access-Control-Allow-Origin
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
 });
 
-app.listen(4000);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const session = require('express-session')
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: {}
+}));
+
+const mongoose = require('mongoose');
+mongoose.connect(CONSTANTS.MONGODB_URL);
+
+require('./users/user-controller')(app);
+
+app.listen(process.env.PORT || 4000);
